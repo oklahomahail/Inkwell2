0a37698dc8f806768524660c8fd280cb354c42c4
oklahomahail <davehail@me.com>
2025-10-15 15:17:35 -0500

    fix: vitest config coverage block

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       'src/services/**/*.{test,spec}.ts',
       'src/components/**/*.{test,spec}.tsx'
     ],
+    coverage: {
       enabled: true,
       provider: 'v8',
       reportsDirectory: './coverage',
